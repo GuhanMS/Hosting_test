48875d794df4913c7a4e315f6b844546bf384690
Guhan <Guhan@INVENGER-502>
2023-04-17 15:17:16 +0530

    Adding only the home pages and removing others

--- URLAuthentication/src/main/java/com/spring/security/controller/EmployeeController.java
+++ URLAuthentication/src/main/java/com/spring/security/controller/EmployeeController.java
@@ -1,51 +1,16 @@
 package com.spring.security.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.spring.security.model.Employee;
-import com.spring.security.repo.EmployeeRepository;
-
 
 @Controller
 public class EmployeeController {
 
-	@RequestMapping("/login")
+	@RequestMapping("/Home")
 	public ModelAndView home() {
-		ModelAndView modelAndView = new ModelAndView("login");
+		ModelAndView modelAndView = new ModelAndView("Home");
 		return modelAndView;
 	}
-	
-//	@Autowired
-//	private EmployeeRepository employeeData;
-//
-//	@RequestMapping(value = "/addNewEmployee.html", method = RequestMethod.POST)
-//	public String newEmployee(Employee employee) {
-//
-//		employeeData.save(employee);
-//		return ("redirect:/listEmployees.html");
-//
-//	}
-//	
-//
-//	@RequestMapping(value = "/addNewEmployee.html", method = RequestMethod.GET)
-//	public ModelAndView addNewEmployee() {
-//
-//		Employee emp = new Employee();
-//		return new ModelAndView("newEmployee", "form", emp);
-//
-//	}
-//
-//	@RequestMapping(value = "/listEmployees.html", method = RequestMethod.GET)
-//	public ModelAndView employees() {
-//		List<Employee> allEmployees = employeeData.findAll();
-//		return new ModelAndView("allEmployees", "employees", allEmployees);
-//
-//	}
 
 }
--- URLAuthentication/src/main/java/com/spring/security/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.security.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.security.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
